859759570818d2c938595622560c75fb36a63c10
Srdjan <stojanac.srdjan@gmail.com>
2025-08-03 20:40:20 +0200

    portfolio modal

---
+++ dist/js/modal.js
@@ -0,0 +1,36 @@
+// PORTFOLIO PROJECT ZOOM - MODAL
+
+const modals = document.querySelectorAll(".modal");
+modals.forEach((modal) => {
+  modal.addEventListener("click", (e) => {
+    e.preventDefault();
+    let body = document.querySelector(".portfolio");
+    let img = e.target.closest(".modal").getAttribute("data-modal");
+    let title = e.target.closest(".modal").getAttribute("data-title");
+    let elem = document.createElement("div");
+    elem.classList.add("x");
+    elem.innerHTML = `
+    <div class="modal__window"> 
+      <div class="modal__wrapper">
+        <img src="${img}" alt="${title}" class="modal__img"/> 
+        <p class="modal__title">
+        <i >
+          ${title}
+        </i>
+        </p>
+        <button class="modal__close">x</button>
+      </div>
+    </div>
+`;
+    body.append(elem);
+    elem.querySelector(".modal__close").addEventListener("click", () => {
+      elem.remove();
+    });
+
+    elem.addEventListener("click", (e) => {
+      if (e.target === elem) {
+        elem.remove();
+      }
+    });
+  });
+});
